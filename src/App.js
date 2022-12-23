1b9c73af89306e22bd01e6ce387daff75c2230b6
Daksh Dagariya <dakshdagariya@Dakshs-MacBook-Air.local>
2022-12-24 02:16:26 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useRef, useEffect } from 'react';
 
 function App() {
+  const [randomInput, setRandomInput] = useState('');
+  const renders = useRef(0);
+  const inputRef = useRef(0);
+  const handleChange = (e) => {
+    setRandomInput(e.target.value);
+    
+  }
+  
+  const changeColor = (e) => {
+    if(inputRef.current.style.color==="blue"){
+      inputRef.current.style.color="red";
+    }
+    else
+    {
+      inputRef.current.style.color="blue";
+    
+      
+    }
+  }
+  useEffect(()=>{
+    renders.current=renders.current+1;
+  })
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="App">
+      <input
+        ref={inputRef}
+        type="text"
+        value={randomInput}
+        placeholder="Random Input"
+        onChange={handleChange}
+      />
+      <p class="render">Renders: {renders.current}</p>
+      <br/> <br/>
+      <section>
+        <button onClick={changeColor}>Change color</button>
+        
+      </section>
+      <br /><br />
+      <br /><br />
+      <p>{randomInput}</p>
+    </main>
   );
 }
 
